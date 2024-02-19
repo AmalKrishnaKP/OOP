67df2cde008df82b6e8587867747b8fd49042fb1
AmalKrishnaKP <148940182+AmalKrishnaKP@users.noreply.github.com>
2024-02-19 19:59:49 +0530

    Create listdemo.java

---
+++ listdemo.java
@@ -0,0 +1,24 @@
+//collection
+import java.util.*;
+import java.io.*;
+
+
+
+class listdemo
+{
+        public static void main(String[]args)
+        {
+                ArrayList<String> list=new ArrayList<String>();
+                list.add("amal");
+                list.add("sarang");
+                list.add("akshith");
+                System.out.println(list);
+
+
+                Iterator it=list.iterator();
+                while(it.hasNext())
+                {
+                        System.out.println(it.next());
+                }
+        }
+}
