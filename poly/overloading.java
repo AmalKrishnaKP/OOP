20f13e2b22525cdaa62159875e09bc7efd7a332d
AmalKrishnaKP <148940182+AmalKrishnaKP@users.noreply.github.com>
2024-02-19 19:47:24 +0530

    Create overloading.java

---
+++ poly/overloading.java
@@ -0,0 +1,28 @@
+//Method overloading
+class a
+{
+        int i;
+        public a(){};
+        public a(int i)
+        {
+                this.i=i;
+        }
+        public a(a one)
+        {
+                this.i=one.i;
+        }
+        public void print()
+        {
+                System.out.println(i);
+        }
+}
+class metd_overloading
+{
+        public static void main(String[]args)
+        {
+                a A1=new a();
+                a A2=new a(5);
+                a A3=new a(A2);
+                A3.print();
+        }
+}
